d4b85c7535fdd51c3085998917b1fbacf50a40b5
ErikManv <erikmanvelyan@yandex.ru>
2023-03-17 12:32:33 +0400

    8 commit

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -3,12 +3,12 @@ package ru.practicum.shareit.booking;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingDtoShort;
 import ru.practicum.shareit.booking.service.BookingService;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
@@ -23,7 +23,7 @@ public class BookingController {
 
     @PostMapping
     public ResponseEntity<BookingDto> addBooking(@RequestHeader("X-Sharer-User-Id") Integer userId,
-                                                @Validated @RequestBody BookingDtoShort bookingDtoShort) {
+                                                @Valid @RequestBody BookingDtoShort bookingDtoShort) {
         return new ResponseEntity<>(bookingService.addBooking(bookingDtoShort, userId), HttpStatus.OK);
     }
 
