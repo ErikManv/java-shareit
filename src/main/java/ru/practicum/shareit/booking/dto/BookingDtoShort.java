56b4bde74961c928e27ec22e0e22a04571389e42
ErikManv <erikmanvelyan@yandex.ru>
2023-03-18 13:00:59 +0400

    13 commit

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDtoShort.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDtoShort.java
@@ -6,24 +6,17 @@ import javax.validation.constraints.Future;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@Setter
-@Getter
+@Data
+@Builder
 public class BookingDtoShort {
-
-    public BookingDtoShort(Integer id, LocalDateTime start, LocalDateTime end) {
-        this.end = end;
-        this.start = start;
-        this.itemId = id;
-    }
-
     @NotNull
     private Integer itemId;
 
     @NotNull
     @Future
-    private final LocalDateTime start;
+    private LocalDateTime start;
 
     @NotNull
     @Future
-    private final LocalDateTime end;
+    private LocalDateTime end;
 }
