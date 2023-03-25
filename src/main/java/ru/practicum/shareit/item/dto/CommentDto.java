f0bac1a79749be3a53355ed89e4dec85d2967680
ErikManv <erikmanvelyan@yandex.ru>
2023-03-25 18:45:11 +0400

    7 commit

--- src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,13 +1,15 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 
-@Data
 @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDto {
 
     private Long id;
