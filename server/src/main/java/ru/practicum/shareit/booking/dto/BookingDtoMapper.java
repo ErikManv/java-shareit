d96545fa20d4cbc59793f1175e09859ae650aee1
ErikManv <erikmanvelyan@yandex.ru>
2023-03-31 11:43:14 +0400

    3 commit

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoMapper.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDtoMapper.java
@@ -2,9 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import org.mapstruct.*;
 import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.booking.model.Status;
-import ru.practicum.shareit.item.dto.ItemMapper;
-import ru.practicum.shareit.user.dto.UserMapper;
 
 @Mapper(componentModel = "spring")
 public interface BookingDtoMapper {
--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestMapper.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestMapper.java
@@ -1,11 +1,8 @@
 package ru.practicum.shareit.request.dto;
 
-import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import ru.practicum.shareit.item.dto.ItemMapper;
 import ru.practicum.shareit.request.model.ItemRequest;
-import ru.practicum.shareit.user.dto.UserMapper;
 
 @Mapper(componentModel = "spring")
 public interface ItemRequestMapper {
